b58798adbf7e4b1f26ec8069c4d1daa0ae39fae1
Roberto Robles <robertodavidrobles@gmail.com>
2016-03-06 17:54:29 -0500

    Pruebas de torres y siglo while para contunuar

--- chess.java
+++ chess.java
@@ -3,22 +3,48 @@ import java.util.Scanner;
 public class chess {
  public static void main(String[] args) {
 
+   boolean flag_again=true;
    int c=0,f=0;
-   String ficha = "";
+   String decide = "";
    Scanner Input = new Scanner(System.in);
    tablero Tablero = new tablero();
-   peon [] Peones = new peon[8];
+   Tablero.start();
    menu Menu = new menu();
 
-   Tablero.start();
-   Tablero.show();
-
+   peon [] Peones = new peon[8];
    for(int i=0;i<Peones.length;i++){
      Peones [i] = new peon (1,i,Tablero.getName(1,i),"P",true);
    }
-   Menu.mov_Ficha(Tablero,Peones);
+
+   torre Torres []= new torre[2];
+   Torres[0] = new torre (0,0,Tablero.getName(0,0),true);
+   Torres[1] = new torre (0,7,Tablero.getName(0,7),true);
+
    Tablero.show();
 
+
+while(flag_again==true){
+  // Menu.mov_Ficha(Tablero,Peones);
+  //Tablero.show();
+
+  Menu.mov_Ficha(Tablero,Torres);
+  Tablero.show();
+
+    System.out.println("deseacontinuar jugando?");
+    decide=Input.next();
+    if(decide.equals("N")||decide.equals("n"))
+    flag_again=false;
+    else if(decide.equals("s")||decide.equals("S"))
+    System.out.println("0K!");
+    else     System.out.println("Que Coño??");
+}
+
+
+
+    //Menu.mov_Ficha(Tablero,Torres);
+    // Tablero.show();
+    //Menu.mov_Ficha(Tablero,Torres);
+    //  Tablero.show();
 /*   System.out.println("digite pocision en x");
    f=Input.nextInt();
    System.out.println("digite pocision en y");
